18f5eaa2aa3f3337e412b43e8f474a9e793cd49f
herreang000 <herreang000@warren.k12.in.us>
2025-01-17 10:59:55 -0500

    final

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Main {
-}
+public class Main {
+    public static void main(String[] args) {
+        int[][] array = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int i, j, sum = 0;
+
+
+        for (i = 0; i <  array.length; ++i) {
+            for (j = 0; j < array[i].length; ++j) {
+                sum += array[i][j];
+            }
+
+            System.out.println("Sum of the row " + i + " = " + sum);
+
+            sum = 0;
+        }
+
+
+        for (i = 0; i <  array.length; ++i) {
+            for (j = 0; j < array[i].length; ++j) {
+                sum += array[i][j];
+            }
+
+            System.out.println("Sum of the column " + i + " = " + sum);
+
+            sum = 0;
+        }
+    }
+
+
+
+    }
+
